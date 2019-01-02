4ba71c1f34b42c68ef649dbd8c979134c59e2fd5
Carlos Garcia <gtxcarl9@gmail.com>
2019-01-02 20:55:40 +0100

    Add Section1 at Component/Index

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import Header from './Header/Index'
+import Header from './Header/Index';
+import Section1 from './Section1/Index';
 class ParallaxApp extends Component {
     render() {
         return (
             <div>
-
+                <Header/>
+                <Section1/>
             </div>
     );
     }
