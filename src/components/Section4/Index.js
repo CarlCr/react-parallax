f63e507db650068f06c88026df39bd190c021639
Carlos Garcia <gtxcarl9@gmail.com>
2019-01-02 20:51:44 +0100

    Adiconar Section4

---
+++ src/components/Section4/Index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class class_name extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default class_name;
